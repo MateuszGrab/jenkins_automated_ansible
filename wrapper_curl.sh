30f854d0cbe656e988e094257171d0ce0812dbb9
Mateusz Grabowski <matgrabowski@gmail.com>
2016-11-28 15:46:27 +0100

    Updated curl simple test

--- wrapper_curl.sh
+++ wrapper_curl.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-curl -X POST http://admin:admin@35.156.82.159:8080/job/api_json/build\?token\=test_token \
+curl -X POST http://admin:test123P4ssw0rd_Jenkins@35.156.82.159:8080/job/api_json/build\?token\=test_token \
   --form json='@curl.json'
